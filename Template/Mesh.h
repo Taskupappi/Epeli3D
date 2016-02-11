111c6e3ac33a0e8cd3ec76658d34fc838ea45993
SikSakJiiPee <juissi93@hotmail.com>
2016-02-11 16:02:30 +0200

    Mesh ja Model luokat

---
+++ Template/Mesh.h
@@ -0,0 +1,117 @@
+//#pragma once
+#ifndef MESH_H
+#define MESH_H
+
+#include <string>
+#include <vector>
+
+#include <sstream>
+
+
+#include <GL/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include "TempShader.h"
+
+struct Vertex
+{
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+struct Texture
+{
+	GLuint id;
+	std::string type;
+};
+
+class Mesh
+{
+public:
+	Mesh(std::vector<Vertex> vertices, std::vector<GLuint> indices, std::vector<Texture> textures)
+	{
+		this->vertices = vertices;
+		this->indices = indices;
+		this->textures = textures;
+
+		this->setupMesh();
+	}
+	//Mesh Data
+	std::vector<Vertex> vertices;
+	std::vector<GLuint> indices;
+	std::vector<Texture> textures;
+	// Functions
+	void Draw(Shader shader)
+	{
+		GLuint diffuseNr = 1;
+		GLuint specularNr = 1;
+		for (GLuint i = 0; i < this->textures.size(); i++)
+		{
+			//Activate proper texture unit before binding
+			glActiveTexture(GL_TEXTURE0 + i); 
+			//Retrieve texture number (the N in diffuse_textureN)
+			std::stringstream ss;
+			std::string number;
+			std::string name = this->textures[i].type;
+			if (name == "texture_diffuse")
+			{
+				//Transder GLuint to stream
+				ss << diffuseNr++;
+			}
+			else if (name == "texture_specular")
+			{
+				//Transder GLuint to stream
+				ss << specularNr++;
+			}
+			number == ss.str();
+
+			glUniform1f(glGetUniformLocation(shader.GetShaderProgram, ("material." + name + number).c_str()), i);
+			glBindTexture(GL_TEXTURE_2D, this->textures[i].id);
+		}
+		glActiveTexture(GL_TEXTURE0);
+
+		//Draw mesh
+		glBindVertexArray(this->VAO);
+		glDrawElements(GL_TRIANGLES, this->indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
+
+private:
+	//Render Data
+	GLuint VAO, VBO, EBO;
+	//Functions
+	void setupMesh()
+	{
+		glGenVertexArrays(1, &this->VAO);
+		glGenBuffers(1, &this->VBO);
+		glGenBuffers(1, &this->EBO);
+
+		glBindVertexArray(this->VAO);
+		glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+
+		glBufferData(GL_ARRAY_BUFFER, this->vertices.size() * sizeof(Vertex),
+			&this->indices[0], GL_STATIC_DRAW);
+
+		//vertex positions
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
+		//Vertex Normals
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+			(GLvoid*)offsetof(Vertex, Normal));
+		//Vertex texture Coords
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex),
+			(GLvoid*)offsetof(Vertex, TexCoords));
+
+		glBindVertexArray(0);
+	}
+};
+
+
+
+
+
+
+#endif
