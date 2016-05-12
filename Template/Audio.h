18b5521949208f1a70322e0a7081355f557dc7d2
Aleksi Nissinen <nissinena@elisanet.fi>
2016-05-12 15:16:21 +0300

    Wav bugfix

--- Template/Audio.h
+++ Template/Audio.h
@@ -19,9 +19,10 @@ public:
 	0 plays the sound once.*/
 	void playSound(int channel, int loops)
 	{
-
 		printf("Playing sound!\n\n");
 		Mix_PlayChannel(channel, _sound, loops);
+		if (Mix_GetError())
+			printf("Audio error: %s\n", Mix_GetError());
 	}
 	/*Set volume for sound.
 	Volume range 0-128.*/
@@ -29,6 +30,8 @@ public:
 	{
 		Mix_VolumeChunk(_sound, volume);
 		printf_s("Volume: %d\n", volume);
+		if (Mix_GetError())
+			printf("Audio error: %s\n", Mix_GetError());
 	}
 	/*Set the position of the sound in relation to the listener.
 	Angle is an integer from 0 to 360, where 0 is due north. Rotates cockwise.
@@ -38,6 +41,8 @@ public:
 		Mix_SetPosition(channel, angle, distance);
 		printf_s("Angle: %d\n", angle);
 		printf_s("Distance: %d\n", distance);
+		if (Mix_GetError())
+			printf("Audio error: %s\n",Mix_GetError());
 	}
 	
 private:
